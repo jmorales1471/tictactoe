b3b83e48a5a9b2371e5f5b547f52095f6b87a13d
John Morales <morales.201@osu.edu>
2017-11-22 19:17:49 -0500

    Initialized Repo

---
+++ src/Game.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Board from './Board'
+import './Game.css';
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b]
+        && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      numFilled: 0,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      numFilled: (this.state.numFilled+1),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    const isDone = this.state.numFilled === 9;
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else if(isDone){
+      status = "Tie! Refresh Page to Start Over"
+    }else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={this.state.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
